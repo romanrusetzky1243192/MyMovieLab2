54e031b27769f3d2c124197bffd1fb8a80c660c7
romanrusetzky1243192 <roman28682972@gmail.com>
2025-05-23 23:44:37 +0700

    создал и настроил

---
+++ app/src/main/java/com/example/mymovielab2/room/LocalDataSource.kt
@@ -0,0 +1,29 @@
+package com.example.mymovielab2.room
+
+import android.app.Application
+import io.reactivex.rxjava3.core.Observable
+import kotlinx.coroutines.rx3.asObservable
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class LocalDataSource(application: Application) {
+
+    private val movieDao = MovieDatabase.getDatabase(application).movieDao()
+
+    val allMovies: Observable<List<MovieEntity>>
+        get() = movieDao.getAllMovies().asObservable()
+
+    fun deleteMovie(movie: MovieEntity) {
+        GlobalScope.launch {
+            movieDao.deleteMovie(movie)
+        }
+    }
+
+    fun insertMovie(movie: MovieEntity) {
+        GlobalScope.launch {
+            movieDao.insertMovie(movie)
+        }
+    }
+
+}
